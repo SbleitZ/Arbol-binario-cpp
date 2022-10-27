312020faf80b55c616596e7ad2b7583a64e9382f
Sbleit <72667973+SbleitZ@users.noreply.github.com>
2022-10-27 20:42:02 -0300

    add remove function

--- Arbol/binarytree.cpp
+++ Arbol/binarytree.cpp
@@ -5,9 +5,6 @@
 ADTbinarytree::ADTbinarytree(){
     _size = 0;
     _height = 0;
-    for(int i=1;i<=M;i++){
-        _container[i] = nullptr;
-    }
 }
 
 
@@ -52,8 +49,25 @@ element_t ADTbinarytree::root(){
 
 //left
 //right
-void ADTbinarytree::remove(element_t){
-
+void ADTbinarytree::remove(element_t n){
+    Arbol *aux;
+    aux = _root;
+    int contador= 0;
+    while(aux != NULL){
+        if(aux->dato == n){//lo encuentra y lo elimina
+            printf("\ndato eliminado : %d\n" ,aux->dato);
+            if(_root->dato == n){
+                _root = _root->left;
+            }
+            free(aux);
+            _size--;
+        }
+        aux = aux->left;
+        contador++;
+    }
+    if(contador == _size){
+        printf("Dato no existe");
+    }
 }
 bool ADTbinarytree::contains(element_t e){
     for(int i=1;i<=M;i++){
